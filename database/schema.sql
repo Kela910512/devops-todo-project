aa16e5e6518fb864ae0c5ebde71725b2faa2aafd
Kelemen László <kelemen.laszlo.adam@gmail.com>
2025-11-28 10:42:08 +0100

    collate2

--- database/schema.sql
+++ database/schema.sql
@@ -1,5 +1,5 @@
 ﻿-- Adatbázis létrehozása
-CREATE DATABASE IF NOT EXISTS todoapp CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS todoapp CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 
 USE todoapp;
 
